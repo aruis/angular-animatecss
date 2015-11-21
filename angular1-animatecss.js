ed9c1ed6ace5b61728d0b6ebb740ca16f3909ce4
aruis <lovearuis@gmail.com>
2015-11-21 09:51:10 +0800

    Initial commit

---
+++ angular1-animatecss.js
@@ -0,0 +1,27 @@
+/**
+ * Created by liurui on 15/11/21.
+ */
+angular.module('angular-animatecss-aruis', [])
+    .directive('animate', function () {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                var animatedClass = 'animated ' + attrs.animate;
+                var isInfinite = attrs.hasOwnProperty('animateInfinite')
+                if (isInfinite) {
+                    animatedClass += ' infinite';
+                }
+
+                element.addClass(animatedClass);
+
+                if (!isInfinite) {
+                    element.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                        element.removeClass(animatedClass);
+                        console.log('dd')
+                    });
+                }
+
+
+            }
+        }
+    });
